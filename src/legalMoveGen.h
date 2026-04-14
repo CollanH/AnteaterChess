d3969abb093f90e6c1b6b35eb39fa16065478209
barbieprogamer <134908745+barbieprogamer@users.noreply.github.com>
2026-04-14 14:37:44 -0700

    HELPER FUNCTIONS FOR chess_types.h in check function finished skeleton for legal move generation program flow

---
+++ src/legalMoveGen.h
@@ -0,0 +1,9 @@
+//
+// Created by clair on 4/14/2026.
+//
+
+#ifndef CHESS22L_LEGALMOVEGEN_H
+#define CHESS22L_LEGALMOVEGEN_H
+#include "chess_types.h"
+	MoveList legalMoveGen(const GameState *gs);
+#endif //CHESS22L_LEGALMOVEGEN_H
